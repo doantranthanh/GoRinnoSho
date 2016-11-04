e247f18a05387a0fbbf3952a2c669b109f377b50
Thanh Doan <t.doan@xlntelecom.co.uk>
2016-11-04 10:46:34 +0000

    Scaffolding JET.Web project

---
+++ JustEastTest/JET.Web.Tests/RoutingTests/RoutingTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Routing;
+using JET.UnityDependency;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace JET.Web.Tests.RoutingTests
+{
+    [TestFixture]
+    public class RoutingTest
+    {
+        [TearDown]
+        public void TearDown()
+        {
+            UnityDependencyContainer.GetCurrent().Clear();
+        }
+
+        [Test]
+        public void Test_Route_To_Home_Page()
+        {
+            var mockContext = MockRepository.GenerateMock<HttpContextBase>();
+            mockContext.Expect(x => x.Request.AppRelativeCurrentExecutionFilePath).Return("~/");
+            var routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            var routeData = routes.GetRouteData(mockContext);
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual("Home", routeData.Values["controller"]);
+        }
+    }
+}
---
+++ JustEastTest/JET.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JET.Web.Global" Language="C#" %>
