af977d3da81248ece1f8a434e33448bc9e2aed28
Thanh Doan <t.doan@xlntelecom.co.uk>
2016-11-04 14:39:44 +0000

    Passed HttpClientService Test

--- JustEastTest/JET.Services.Implementations.Tests/HttpClientServicesTests.cs
+++ JustEastTest/JET.Services.Implementations.Tests/HttpClientServicesTests.cs
@@ -1,9 +1,28 @@
-﻿using NUnit.Framework;
+﻿using System.Dynamic;
+using JET.Services.Implementations.WebClient;
+using JET.Services.Interfaces.WebClient;
+using NUnit.Framework;
 
 namespace JET.Services.Implementations.Tests
 {
     [TestFixture]
     public class HttpClientServicesTests
     {
+        private IHttpClientService _httpClientServices;
+
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+
+        [Test]
+        public void Existing_HttpClientService()
+        {
+            _httpClientServices = new HttpClientService();
+            Assert.IsNull(_httpClientServices);
+            Assert.IsInstanceOf<IHttpClientService>(_httpClientServices);
+        }
     }
 }
---
+++ JustEastTest/JET.Services.Implementations/WebClient/HttpClientService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JET.Services.Interfaces.WebClient;
+
+namespace JET.Services.Implementations.WebClient
+{
+    public class HttpClientService: IHttpClientService
+    {
+    }
+}
