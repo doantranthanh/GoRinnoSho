0bc2d9f77b286ca9afea723082f9ed7d09a81eaa
Thanh Doan <t.doan@xlntelecom.co.uk>
2016-11-04 17:26:08 +0000

    Init Routing Test

---
+++ JustEastTest/JET.WebApi.Tests/WebApiRoutingTests/RoutingTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using NUnit.Framework;
+
+namespace JET.WebApi.Tests.WebApiRoutingTests
+{
+    [TestFixture]
+    public class RoutingTests
+    {    
+        private HttpConfiguration _httpConfiguration;
+
+        [SetUp]
+        public void SetUp()
+        {           
+            _httpConfiguration = new HttpConfiguration();
+            WebApiConfig.Register(_httpConfiguration);
+            _httpConfiguration.EnsureInitialized();
+        }
+       
+    }  
+}
