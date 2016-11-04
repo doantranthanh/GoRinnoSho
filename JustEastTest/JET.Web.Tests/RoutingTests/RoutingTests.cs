b697cdb15db5ac0e5d4868bd214e545aab75b465
Thanh Doan <t.doan@xlntelecom.co.uk>
2016-11-04 10:50:34 +0000

    Pass Routing Tests

--- JustEastTest/JET.Web.Tests/RoutingTests/RoutingTests.cs
+++ JustEastTest/JET.Web.Tests/RoutingTests/RoutingTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web;
 using System.Web.Routing;
 using JET.UnityDependency;
+using JET.Web.App_Start;
 using NUnit.Framework;
 using Rhino.Mocks;
 
--- JustEastTest/JET.Web/Global.asax.cs
+++ JustEastTest/JET.Web/Global.asax.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
+using System.Web.Mvc;
+using System.Web.Routing;
+using JET.Web.App_Start;
 
 namespace JET.Web
 {
@@ -12,37 +10,9 @@ namespace JET.Web
 
         protected void Application_Start(object sender, EventArgs e)
         {
-
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
-
-        protected void Session_Start(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_AuthenticateRequest(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_Error(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Session_End(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_End(object sender, EventArgs e)
-        {
-
-        }
+      
     }
 }
