611455b227e43de1bb19dcb67d55bf75b662878c
Thanh Doan <t.doan@xlntelecom.co.uk>
2016-11-04 10:52:17 +0000

    Add HomeController Tests

---
+++ JustEastTest/JET.Web.Tests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+using StructureMap.AutoMocking;
+
+namespace JET.Web.Tests.ControllerTests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private RhinoAutoMocker<HomeController> _mockHomeController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockHomeController = new RhinoAutoMocker<HomeController>();
+        }
+
+        [Test]
+        public void IndexTests()
+        {
+            // Act
+            var result = _mockHomeController.ClassUnderTest.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
