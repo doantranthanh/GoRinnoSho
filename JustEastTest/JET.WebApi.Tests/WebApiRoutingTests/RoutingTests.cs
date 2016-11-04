5f8402705bed383ce831fb0986134d24e4f7e1e0
Thanh Doan <t.doan@xlntelecom.co.uk>
2016-11-04 17:39:20 +0000

    Passed Web API Routing Tests

--- JustEastTest/JET.WebApi.Tests/WebApiRoutingTests/RoutingTests.cs
+++ JustEastTest/JET.WebApi.Tests/WebApiRoutingTests/RoutingTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http;
+using System.Web.Http;
+using JET.WebApi.Controllers;
+using MvcRouteTester;
 using NUnit.Framework;
 
 namespace JET.WebApi.Tests.WebApiRoutingTests
@@ -15,6 +18,14 @@ namespace JET.WebApi.Tests.WebApiRoutingTests
             WebApiConfig.Register(_httpConfiguration);
             _httpConfiguration.EnsureInitialized();
         }
-       
+
+        [Test]
+        [TestCase("q=cr8&c=&name=")]
+        public void GetRestaurants_WithCorrectRoute_CallsAppropriateMethod(string stringQuery)
+        {
+            const string route = "/api/restaurant/q=cr8&c=&name=";
+            RouteAssert.HasApiRoute(_httpConfiguration, route, HttpMethod.Get);
+            _httpConfiguration.ShouldMap(route).To<RestaurantController>(HttpMethod.Get, x => x.GetRestaurants(stringQuery));
+        }
     }  
 }
---
+++ JustEastTest/JET.WebApi/Controllers/RestaurantController.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace JET.WebApi.Controllers
+{
+    [RoutePrefix("api")]
+    public class RestaurantController : ApiController
+    {
+        [Route("restaurant/{queryString}")]
+        [HttpGet]
+        public HttpResponseMessage GetRestaurants(string queryString = null)
+        {
+            return null;
+        }
+    }
+}
