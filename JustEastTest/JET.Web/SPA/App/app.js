4f281b5a3df08ee504dce7619a7028ba7af235f2
Thanh Doan <t.doan@xlntelecom.co.uk>
2016-11-07 17:04:18 +0000

    Init App.js for SPA JET.Web

---
+++ JustEastTest/JET.Web/SPA/App/app.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    'use strict';
+
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+          .when('/restaurants/details/v1', {
+              templateUrl: '/signuponline/Spa/Templates/productOptionsMainPage.html',
+              controller: 'productOptionsController'
+          }).when('/restaurants/details/v2', {
+              templateUrl: '/signuponline/Spa/Templates/yourOrderMainPage.html',
+              controller: 'yourOrderController'
+          }).otherwise({
+              redirectTo: '/'
+          });
+       
+        $locationProvider.html5Mode(true);    
+    }
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+
+    function run($rootScope, $location, $route, $timeout, $window) {
+        $rootScope.config = {};
+        $rootScope.config.app_url = $location.url();
+        $rootScope.config.app_path = $location.path();
+        $rootScope.layout = {};
+        $rootScope.layout.loading = false;
+
+        $rootScope.$on('$routeChangeStart', function () {
+            $window.scrollTo(0, 0);
+           
+        });
+        $rootScope.$on('$routeChangeSuccess', function () { 
+           
+        });
+        $rootScope.$on('$routeChangeError', function () {
+            
+        }); 
+    }
+
+    run.$inject = ['$rootScope', '$location', '$route', '$timeout', '$window'];
+
+    angular.module('justEastTesting', ['common.core', 'common.ui'])
+        .config(config)
+        .run(run);
+
+})();
+
