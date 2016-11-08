d3c46ea27d1009ba0dd09b0ebb7386388b7c00e2
Thanh Doan <t.doan@xlntelecom.co.uk>
2016-11-08 13:22:10 +0000

    Postcode validation directive

--- JustEastTest/JET.Web/SPA/Directives/postcodeValidator.directive.js
+++ JustEastTest/JET.Web/SPA/Directives/postcodeValidator.directive.js
@@ -3,7 +3,7 @@
 
     function postcodeValidator() {
         var fullPostcodeRegexp = /^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/;
-        var postCodeLeftRegexp = /^[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]? {0,1}$/;
+        var postCodeOutWardRegexp = /^[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]? {0,1}$/;
 
         return {
             restrict: 'A',
@@ -11,19 +11,21 @@
             link: function(scope, element, attributes, ngModel) {
                 ngModel.$validators.postcodeValidator = function(modelValue) {
                     if (modelValue.length !== 0) {
-                        if (modelValue.length === 3) {
-                            if (postCodeLeftRegexp.test(modelValue)) {
+                        if (modelValue.length >= 2 && modelValue.length <= 4) {
+                            if (postCodeOutWardRegexp.test(modelValue)) {
                                 return true;
                             } else {
                                 return false;
                             }
                         }
-                        if (modelValue.length > 3) {
+                        if (modelValue.length > 4 && modelValue.length <= 8) {
                             if (fullPostcodeRegexp.test(modelValue)) {
                                 return true;
                             } else {
                                 return false;
                             }
+                        } else {
+                            return false;
                         }
                     } else {
                         return true;
