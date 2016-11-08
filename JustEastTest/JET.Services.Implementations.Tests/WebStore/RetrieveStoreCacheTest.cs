53c281d48255daedaebbc232f268039d667d0a3e
Thanh Doan <t.doan@xlntelecom.co.uk>
2016-11-08 11:33:58 +0000

    Init first test for cacheHelper

--- JustEastTest/JET.Services.Implementations.Tests/WebStore/RetrieveStoreCacheTest.cs
+++ JustEastTest/JET.Services.Implementations.Tests/WebStore/RetrieveStoreCacheTest.cs
@@ -1,4 +1,6 @@
-﻿using JET.Services.Interfaces.WebStore;
+﻿using System.Runtime.Caching;
+using JET.Services.Implementations.WebStore;
+using JET.Services.Interfaces.WebStore;
 using NUnit.Framework;
 
 namespace JET.Services.Implementations.Tests.WebStore
@@ -12,6 +14,16 @@ namespace JET.Services.Implementations.Tests.WebStore
         public void SetUp()
         {
             _cacheHelper = new CacheHelper();
+        }
+
+        [Test]
+        public void Test_Get_Object_From_Cache_If_Cache_Contain_Data()
+        {
+            ObjectCache cache = MemoryCache.Default;
+            _cacheHelper.StoreObjToCache("key", "jet testing");
+            var obj = _cacheHelper.RetrieveCacheObj<string>("key");
+            Assert.AreEqual("jet testing", obj);
+            cache.Remove("key");
         }
     }
 }
---
+++ JustEastTest/JET.Services.Implementations/WebStore/CacheHelper.cs
@@ -0,0 +1,8 @@
+﻿using JET.Services.Interfaces.WebStore;
+
+namespace JET.Services.Implementations.WebStore
+{
+    public class CacheHelper : ICacheHelper
+    {
+    }
+}
