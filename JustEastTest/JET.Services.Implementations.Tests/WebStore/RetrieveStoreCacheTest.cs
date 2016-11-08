b141f21d184bc2c0b83ac05ce6965eb78f197960
Thanh Doan <t.doan@xlntelecom.co.uk>
2016-11-08 11:28:27 +0000

    Init CacheHelper test

---
+++ JustEastTest/JET.Services.Implementations.Tests/WebStore/RetrieveStoreCacheTest.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace JET.Services.Implementations.Tests.WebStore
+{
+    [TestFixture]
+    public class RetrieveStoreCacheTest
+    {
+        private ICacheHelper _cacheHelper;
+    }
+}
